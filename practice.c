987bdeccabae3ed215b80dbe9e89c8002f6525c3
J Carlos <swali@gmail.com>
2022-11-26 10:30:40 -0330

    finished project

--- practice.c
+++ practice.c
@@ -2,21 +2,40 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-#define MEMORY_SIZE 20
+#define MEMORY_SIZE 15
 
+typedef enum {false, true} bool;
+typedef struct {
+  int id;
+  char name[20];
+  bool state;
+  int size;
+  int located;
+  char date[20];
+} Process;
+typedef struct {
+  Process *array;
+  size_t used;
+  size_t size;
+} Array;
+
+
+/*
+ * Global variables
+ */
 int memory[MEMORY_SIZE] = {0};
+Array processTable;
+int serial = 0;
 
 /** Colors to print on console **/
 void reset () {
   printf("\033[0m");
 }
-
 void printRed (const char *format) {
 	printf("\033[1;31m");
-	printf("\n");
-	printf("%s", format);
-	printf("\n");
+	printf("\n%s\n", format);
 	reset();
 }
 void printCyan (const char *format) {
@@ -24,12 +43,87 @@ void printCyan (const char *format) {
 	printf("%s", format);
 	reset();
 }
+void yellow() {
+	printf("\033[0;33m");
+}
+void green() {
+	printf("\033[0;32m");
+}
+void printBlue(const char *format) {
+	printf("\033[0;34m");
+	printf("%s", format);
+	reset();
+}
+void printGreen(const char *format) {
+	printf("\033[0;32m");
+	printf("%s", format);
+	reset();
+}
 
 /*
- * Function to clear console 
+ * Manage dymanic lists
  */
-int clearConsole() {
-	printf("\e[1;1H\e[2J");
+void initArray(Array *a, size_t initialSize) {
+  a->array = malloc(initialSize * sizeof(Process));
+  a->used = 0;
+  a->size = initialSize;
+}
+void insertArray(Array *a, Process element) {
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = realloc(a->array, a->size * sizeof(Process));
+  }
+
+  a->array[a->used++] = element;
+}
+
+/*
+ * Print on console Memory Used
+ */
+void showMemory() {
+  printf(" "); 
+  for(int i = 0; i < MEMORY_SIZE; i++) {
+    if(i != MEMORY_SIZE-1) printf("─────");
+    else printf("────\n");
+  }
+
+  printf("│");
+  for(int i = 0; i < MEMORY_SIZE; i++) {
+
+		if(memory[i] == -1) printf(" OS │");
+		else printf(" %-2d │", memory[i]);
+  }
+  printf("\n");
+
+  for(int i = 0; i < MEMORY_SIZE; i++) {
+    if(i == 0) printf(" "); 
+
+    if(i != MEMORY_SIZE-1) printf("─────");
+    else printf("────\n");
+  }
+}
+
+/*
+ * Print on console Process Talbe
+ */
+void showProcessTable() {
+  printf(" ─────────────────────────────────────────────────────────────\n"); 
+  printf("│ id │  Name      │ State    │ Space │ Located │ Start On     │\n"); 
+  printf("│─────────────────────────────────────────────────────────────│\n"); 
+
+  for(int i = 0; i < processTable.used; i++) {
+    printf(
+      "│ %-2d │ %-10s │ %-8s │ %-5d | %-7d | %-12s │\n",
+      processTable.array[i].id,
+      processTable.array[i].name,
+      processTable.array[i].state == true ? "active" : "finished",
+      processTable.array[i].size,
+      processTable.array[i].located,
+      processTable.array[i].date
+    ); 
+  }
+
+  printf(" ─────────────────────────────────────────────────────────────\n"); 
 }
 
 int getFreeSpace() {
@@ -40,52 +134,95 @@ int getFreeSpace() {
 		else break;
 	}
 
-
 	return free;
 }
 
 /*
  * Function to add new process 
  */
+void addProcessToTable(int pid, int psize, int located) {
+	Process process;
+	struct tm* date;
+	time_t timeRef;
+	char name[20];
+	char dateStr[20];
+
+	timeRef = time(NULL);
+	date = localtime(&timeRef);
+
+	sprintf(
+		dateStr,
+		"%d:%d:%d",
+		date->tm_hour,
+		date->tm_min,
+		date->tm_sec
+	);
+	sprintf(name, "process %d", pid);
+
+	process.id = pid; 
+	process.size = psize; 
+	process.state = true; 
+	process.located = located; 
+	strcpy(process.name, name); 
+	strcpy(process.date, dateStr); 
+
+	insertArray(&processTable, process);
+}
+
 int addProcess() {
-	int processSize = rand() % 10;  
+	int limit = (int)(MEMORY_SIZE /2);
+	int processSize = rand() % limit;  
 	if(processSize == 0) processSize = 1;
 
 	int freeSize = getFreeSpace();
 
-	//printf("free: %d \n", freeSize);
-
 	if(freeSize >= processSize) {
-		int i, start = MEMORY_SIZE - freeSize, end = start + processSize;
+		int start = MEMORY_SIZE - freeSize;
+		int end = start + processSize;
+		int pid = ++serial;
 
-		for(i = start; i < end; i++) {
-			memory[i] = processSize;
+		for(int i = start; i < end; i++) {
+			memory[i] = pid;
 		}
+
+		addProcessToTable(pid, processSize, start);
+
+		green();
+		printf("\nProcess(%d) added with %d space(s)\n\n", pid, processSize);
+		reset();
 	} else {
-		printCyan("\n\n\nNo have memory space\n\n\n");
+		yellow();
+		printf("\nWe no have enough memory space: %d(u)\n\n", processSize);
+		reset();
 	}
 }
 
 /*
- * Function to add  
- */
-int showMemory() {
-	int i;
-
-	for(i = 0; i < MEMORY_SIZE; i++) {
-		printf("position: %d, value: %d\n", i+1, memory[i]);
-	}
-}
-
-/*
- * Function to add  
+ *  Compact Memory 
  */
 int compactMemory() {
-	int i;
-
-	for(i = 0; i < MEMORY_SIZE; i++) {
-		printf("position: %d, value: %d\n", i+1, memory[i]);
+	for(int i = 2; i < MEMORY_SIZE; i++) {
+		memory[i] = 0;
 	}
+
+	for(int i = 0; i < processTable.used; i++) {
+		Process curr = processTable.array[i]; 
+
+		if(curr.state == true) {
+			int freeSize = getFreeSpace();
+			int start = MEMORY_SIZE - freeSize;
+			int end = start + curr.size;
+
+			int pid = curr.id;
+
+			for(int i = start; i < end; i++) {
+				memory[i] = pid;
+			}
+
+			processTable.array[i].located = start;
+		}
+	}
+		printGreen("\nSuccessful compacted memory\n\n");
 }
 
 /*
@@ -93,7 +230,7 @@ int compactMemory() {
  */
 int showManual() {
 	printf(
-		"0) ➛ Exit\n"
+		"0) ➛ Show options\n"
 		"1) ➛ Generate rambom process\n"
 		"2) ➛ Finish process\n"
 		"3) ➛ Compact processes\n"
@@ -106,51 +243,63 @@ int showManual() {
  * Function to show manual
  */
 void terminateProcess() {
-	int option, extra;
+	int pid = 0, extra;
 
-	printCyan("Enter the pid: ");
+	showMemory(); 
 
-	option = getchar();
+	printBlue("Enter the pid [ENTER to cancel]: ");
+	while((extra = getchar()) != '\n') {
+		int digit = extra - 0x30;
+		pid =  (pid * 10) + digit;
+	}
 
-	if(option == '\n') return;
-	while((extra = getchar()) != '\n') { }
+	if (pid != 0) {   
+		for(int i = 0; i < MEMORY_SIZE; i++) {
+			if(memory[i] == pid) memory[i] = 0;
+		}
 
-	for(i = 0; i < MEMORY_SIZE; i++) {
-		printf("position: %d, value: %d\n", i+1, memory[i]);
+		for(int i = 0; i < processTable.used; i++) {
+			if(processTable.array[i].id == pid) {
+				processTable.array[i].state = false;
+				processTable.array[i].located = -1;
+			}
+		}
+
+		printf("\nProcess %d finished.\n\n", pid);
 	}
 }
 
 int main(void) {
 	int option, extra;
 
+  initArray(&processTable, 10);
 	srand(time(NULL));
 
+	showManual();
 	while(1) {
-		showManual();
 		printCyan("Please enter an option: ");
 
 		option = getchar();
+		
+		memory[0] = -1;
+		memory[1] = -1;
 
 		if(option == '\n') continue;
 		while((extra = getchar()) != '\n') { }
 
-		if(option != '\n') {
-			if(option == '0')
-				exit(EXIT_SUCCESS);
-			else if(option == '1')
-				addProcess();
-			else if(option == '2')
-				terminateProcess();
-			else if(option == '3')
-				compactMemory(); 
-			else if(option == '4')
-				showMemory(); 
-			else
-				printRed(
-					"** Invalid option *** \n\n"
-					"Enter 6 to cheack available options"
-				);
-		}
+		if(option == '0')
+			showManual();
+		else if(option == '1')
+			addProcess();
+		else if(option == '2')
+			terminateProcess();
+		else if(option == '3')
+			compactMemory(); 
+		else if(option == '4')
+			showProcessTable(); 
+		else if(option == '5')
+			showMemory(); 
+		else showManual();
 	}
 
 	return(0);
--- process.c
+++ process.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// struct process {
-//   int id;
-//   char name;
-// };
-
-typedef struct {
-  int id;
-  char name;
-} Process;
-
-typedef struct {
-  Process *array;
-  size_t used;
-  size_t size;
-} Array;
+#include <string.h>
+#include <time.h>
 
 void initArray(Array *a, size_t initialSize) {
-  a->array = malloc(initialSize * sizeof(int));
+  a->array = malloc(initialSize * sizeof(Process));
   a->used = 0;
   a->size = initialSize;
 }
@@ -26,33 +12,29 @@ void initArray(Array *a, size_t initialSize) {
 void insertArray(Array *a, Process element) {
   if (a->used == a->size) {
     a->size *= 2;
-    a->array = realloc(a->array, a->size * sizeof(int));
+    a->array = realloc(a->array, a->size * sizeof(Process));
   }
 
   a->array[a->used++] = element;
 }
 
-void freeArray(Array *a) {
-  free(a->array);
-  a->array = NULL;
-  a->used = a->size = 0;
-}
 
 int main() {
-  Process s1;
   Array a;
-  s1.name = 'x';
-  s1.id = 1;
+  int typed;
+  int serial = 0;
 
   initArray(&a, 10);
 
-  insertArray(&a, s1);
+  while(1) {
+    scanf("%d", &typed);
 
-  printf("%ld\n", a.used); 
+    if(typed == 0) {
+    } else {
+      showProcessTable();
+    }
+  }
 
-  freeArray(&a);
-
-  printf("%ld\n", a.used); 
 
   return 0;
 }
